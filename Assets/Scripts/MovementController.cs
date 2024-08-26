20cf860bf00444aa24ce76ea1c8ba83702a48c08
Yasho-Nandan-Reddy <sangireddy.ynr@gmail.com>
2024-08-27 01:03:15 +0530

    .

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -6,12 +6,19 @@ public class MovementController : MonoBehaviour
 {
     public float movementspeed = 5.0f;
     public float rotationspeed = 2.0f;
+    [SerializeField] private GameObject pulpit;
+    private GameObject pulpitinstance;
+    public float pulpittime = 5.0f;
 
     private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Vector3 spawnpoint= new Vector3(0, 0, 0);
+        pulpitinstance = Instantiate(pulpit, spawnpoint, Quaternion.identity);
     }
 
     void HandleMovement()
@@ -19,7 +26,7 @@ public class MovementController : MonoBehaviour
         float forward= Input.GetAxis("Vertical");
         float rotation = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(0, 0, forward) * movementspeed * Time.deltaTime;
+        Vector3 movement = new Vector3(0, 0, forward) * movementspeed * (Time.deltaTime * 2);
         rb.MovePosition(transform.position + transform.TransformDirection(movement));
 
         Quaternion turn = Quaternion.Euler(0, rotation * rotationspeed * Time.deltaTime, 0);
@@ -30,5 +37,6 @@ public class MovementController : MonoBehaviour
     void Update()
     {
         HandleMovement();
+        Destroy(pulpitinstance, pulpittime);
     }
 }
